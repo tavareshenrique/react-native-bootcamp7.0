587b7e766d099aa13c35cdfc6fcfafe177903b34
Henrique Tavares <ihenrits@gmail.com>
2019-08-14 00:20:00 -0300

    React Navigation

--- android/app/src/main/java/com/reactnativebootcamp07/MainActivity.java
+++ android/app/src/main/java/com/reactnativebootcamp07/MainActivity.java
@@ -1,6 +1,9 @@
 package com.reactnativebootcamp07;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "reactnativebootcamp07";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+          return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
     }
 }
