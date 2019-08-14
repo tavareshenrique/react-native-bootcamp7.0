e14edcc2d9f03e86951a7d15d89b16f44ba8ed01
Henrique Tavares <ihenrits@gmail.com>
2019-08-14 13:02:09 -0300

    Acessando API do Github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState('');
+
+  async function handleAddUser() {
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  }
+
   return (
     <Container>
       <Form>
@@ -12,8 +32,12 @@ export default function Main() {
           autoCorrect={false}
           autoCapitalize="none"
           placeholder="Adicionar Usuario"
+          value={newUser}
+          onChangeText={text => setNewUser(text)}
+          returnKeyType="send"
+          onSubmitEditing={() => handleAddUser()}
         />
-        <SubmitButton>
+        <SubmitButton onPress={() => handleAddUser()}>
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
